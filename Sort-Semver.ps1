6586086b837476489829469f59207b6df538b4f2
Klas Mellbourn <klas@mellbourn.net>
2014-10-12 18:16:08 +0200

    using regexes

--- Sort-Semver.ps1
+++ Sort-Semver.ps1
@@ -1,15 +1,31 @@
-param (
-  [switch] $PreRelease
-)
+#param (
+#  [switch] $PreRelease
+#)
 $suffix = ""
-if($PreRelease) {
-  $suffix = "-"
-}
+#if($PreRelease) {
+#  $suffix = "-"
+#}
 
 Function CompareTo ($adir, $bdir) 
 {
   $a = $adir.Name
   $b = $bdir.Name
+
+  $semverregex = "^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$"
+  $a -match $semverregex
+  [int]$amajor = $Matches[1]
+  [int]$aminor = $Matches[2]
+  [int]$apatch = $Matches[3]
+  [int]$apatch = $Matches[3]
+  $aprerelease = $Matches[4]
+  Write-Host $a $amajor + $aminor + $apatch + $aprerelease
+
+  $b -match $semverregex
+  [int]$bmajor = $Matches[1]
+  [int]$bminor = $Matches[2]
+  [int]$bpatch = $Matches[3]
+  [int]$bpatch = $Matches[3]
+  $bprerelease = $Matches[4]
 
   if( $a -lt $b) 
   {
