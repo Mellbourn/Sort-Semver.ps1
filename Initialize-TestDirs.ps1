f429abeb70037a0f616c168384491e8d23a3076c
Klas Mellbourn <klas@mellbourn.net>
2014-10-12 17:01:03 +0200

    better suffix handling

---
+++ Initialize-TestDirs.ps1
@@ -0,0 +1,8 @@
+mkdir 1.0
+mkdir 4.0.3
+mkdir 6.0.0
+mkdir 51.0.0
+mkdir 51.0.0-alpha.0+soft
+mkdir 52.0.4
+mkdir 52.0.11
+mkdir 52.0.256
--- Sort-Semver.ps1
+++ Sort-Semver.ps1
@@ -1,5 +1,12 @@
+param (
+  [switch] $PreRelease
+)
 # började 16:37
-$versionDirs = Get-ChildItem -Recurse | Where-Object { $_.PSIsContainer }
+$suffix = ""
+if($PreRelease) {
+  $suffix = "-"
+}
+$versionDirs = Get-ChildItem -Recurse | Where-Object { $_.PSIsContainer } | Where-Object { $_.Name -match ("^[0-9]+\.[0-9]+\.[0-9]+" + $suffix) }
 $sortedDirs = $versionDirs | Select-Object -ExpandProperty Name | % { New-Object -TypeName System.Version -ArgumentList $_ } | Sort-Object
 Write-Host $versionDirs
 Write-Host $sortedDirs
